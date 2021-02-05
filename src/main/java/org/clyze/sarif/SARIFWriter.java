1d1cd61f596127a7559f7433cbd13ee2fbc8ae5c
George Fourtounis <gfour@di.uoa.gr>
2021-02-05 16:45:58 +0200

    Use pretty printer for JSON output.

--- src/main/java/org/clyze/sarif/SARIFWriter.java
+++ src/main/java/org/clyze/sarif/SARIFWriter.java
@@ -6,8 +6,18 @@ import org.clyze.sarif.model.SARIF;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * The main entry points of the library.
+ */
+@SuppressWarnings("unused")
 public class SARIFWriter {
+    /**
+     * Write a SARIF object to a file.
+     * @param sarif          the SARIF object to write
+     * @param sarifOut       the target file
+     * @throws IOException   on file write error
+     */
     public static void write(SARIF sarif, File sarifOut) throws IOException {
-        (new ObjectMapper()).writeValue(sarifOut, sarif.toMap());
+        (new ObjectMapper()).writerWithDefaultPrettyPrinter().writeValue(sarifOut, sarif.toMap());
     }
 }
