e745e8cb6b52b7eef305648778b61975f961bcb2
George Fourtounis <gfour@di.uoa.gr>
2021-02-05 16:46:20 +0200

    Add higher-level write() method.

--- src/main/java/org/clyze/sarif/SARIFWriter.java
+++ src/main/java/org/clyze/sarif/SARIFWriter.java
@@ -1,10 +1,13 @@
 package org.clyze.sarif;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.clyze.sarif.model.SARIF;
+import org.clyze.sarif.model.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * The main entry points of the library.
@@ -19,5 +22,24 @@ public class SARIFWriter {
      */
     public static void write(SARIF sarif, File sarifOut) throws IOException {
         (new ObjectMapper()).writerWithDefaultPrettyPrinter().writeValue(sarifOut, sarif.toMap());
+    }
+
+    /**
+     * Write analysis results to a file.
+     * @param name         the simple name of the analysis tool
+     * @param fullName     the full name of the analysis tool
+     * @param version      the version
+     * @param sarifOut     the target file
+     * @param rules        the list of analysis rules
+     * @param results      the list of analysis results
+     * @throws IOException on file write error
+     */
+    public static void write(String name, String fullName, String version,
+                             File sarifOut, List<Rule> rules, List<Result> results)
+            throws IOException {
+        Driver driver = new Driver(name, fullName, version, "1.0", rules);
+        Run run = new Run(results, new ArrayList<>(), new Tool(driver));
+        SARIF sarif = new SARIF(Collections.singletonList(run));
+        write(sarif, sarifOut);
     }
 }
